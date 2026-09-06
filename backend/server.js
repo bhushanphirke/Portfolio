ddbafdd802e54390a92d9562c0a08e45e4a577c4
Bhushan Phirke <bhushanphirke314@gmail.com>
2026-09-07 01:52:31 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const nodemailer = require('nodemailer');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // ---------- Middleware ----------
